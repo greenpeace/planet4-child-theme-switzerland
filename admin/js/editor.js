516e2f8bd3af5e3b7917ebe886e0eeeff85ab507
Stefan Dürrenberger <stduerre@greenpeace.org>
2019-05-10 10:05:13 +0200

    Added heading block styles

--- admin/js/editor.js
+++ admin/js/editor.js
@@ -1,5 +1,22 @@
 wp.domReady( () => {
 
+	// core/heading
+	wp.blocks.registerBlockStyle( 'core/heading', {
+		name: 'default',
+		label: 'Default',
+		isDefault: true,
+	} );
+
+	wp.blocks.registerBlockStyle( 'core/heading', {
+		name: 'underline',
+		label: 'Underline',
+	} );
+
+	wp.blocks.registerBlockStyle( 'core/heading', {
+		name: 'block',
+		label: 'Block',
+	} );
+
 	// core/list
 	wp.blocks.registerBlockStyle( 'core/list', {
 		name: 'default',
