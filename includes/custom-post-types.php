a6721913a1dffe44e0c90b25546340695994998f
Stefan Dürrenberger <stduerre@greenpeace.org>
2019-06-17 15:03:31 +0200

    247: Gutenberg editor for jobs

--- includes/custom-post-types.php
+++ includes/custom-post-types.php
@@ -69,6 +69,7 @@ if ( ! function_exists( 'p4_child_theme_gpch_custom_post_job' ) ) {
 			'publicly_queryable'  => true,
 			'rewrite'             => $rewrite,
 			'capabilities'        => $capabilities,
+			'show_in_rest'        => true, // needed  for Gutenberg editor
 		);
 
 		register_post_type( 'gpch_job', $args );
