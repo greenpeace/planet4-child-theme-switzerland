6cfb67e79bfb32b20e8fd56c686e00f0c8cb1951
Stefan Dürrenberger <stefan.duerrenberger@greenpeace.org>
2020-11-23 12:11:05 +0100

    Add basic REST API method

--- classes/GPCH_i_INXMAIL_API.php
+++ classes/GPCH_i_INXMAIL_API.php
@@ -5,5 +5,44 @@
  * Class GPCH_i_INXMAIL_API
  */
 class GPCH_i_INXMAIL_API {
+	/**
+	 * @param $method: POST, PUT, GET etc
+	 * @param $url
+	 * @param bool $data: array("param" => "value") ==> index.php?param=value
+	 *
+	 * @return bool|string
+	 */
+	function CallAPI( $method, $url, $data = false ) {
+		$curl = curl_init();
 
+		switch ( $method ) {
+			case "POST":
+				curl_setopt( $curl, CURLOPT_POST, 1 );
+
+				if ( $data ) {
+					curl_setopt( $curl, CURLOPT_POSTFIELDS, $data );
+				}
+				break;
+			case "PUT":
+				curl_setopt( $curl, CURLOPT_PUT, 1 );
+				break;
+			default:
+				if ( $data ) {
+					$url = sprintf( "%s?%s", $url, http_build_query( $data ) );
+				}
+		}
+
+		// Optional Authentication:
+		curl_setopt( $curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC );
+		curl_setopt( $curl, CURLOPT_USERPWD, "username:password" );
+
+		curl_setopt( $curl, CURLOPT_URL, $url );
+		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
+
+		$result = curl_exec( $curl );
+
+		curl_close( $curl );
+
+		return $result;
+	}
 }
