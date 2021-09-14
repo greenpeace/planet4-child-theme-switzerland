e68ebd6e93a8d74a3f3aa8cfd5cd5a017f26ae44
Stefan D <stefan.duerrenberger@greenpeace.org>
2021-09-14 14:01:53 +0200

    527: Add option for CSP allowlist entries

---
+++ includes/csp.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Set Content Security Policy HTTP headers to allow embedding of gravity forms
+ *
+ * See GravityForms extensions for separate allowlist functionality
+ */
+function gpch_cps_allowlist( $allowlist ) {
+	global $wp;
+
+	$options = get_option( 'gpch_child_options' );
+
+	$allowed_ancestors = preg_split( '/\r\n|\r|\n/', $options['gpch_child_field_content_embed_allowlist'] );
+
+	return array_merge( $allowlist, $allowed_ancestors );
+}
+
+add_filter( 'planet4_csp_allowed_frame_ancestors', 'gpch_cps_allowlist' );
