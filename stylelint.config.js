e3bfaa52109d076cda69d175feefaaf5289078d5
Stefan Dürrenberger <sd@pm7.ch>
2025-12-16 16:54:18 +0100

    ES-325: Add Styleline config

---
+++ stylelint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	extends: "@wordpress/stylelint-config/stylistic",
+	rules: {
+		"comment-empty-line-before": ["always", { "except": ["first-nested"] }],
+		"selector-id-pattern": null,
+		"selector-class-pattern": null,
+		"@stylistic/max-line-length": null,
+		"rule-empty-line-before": ["always", {"except": ["first-nested"], "ignore": ["after-comment"]}],
+		"at-rule-no-unknown": [true, {"ignoreAtRules": ["include"]}],
+		"no-descending-specificity": null
+	},
+	ignoreFiles: ["style.css", "admin/css/editor-style.css"]
+};
