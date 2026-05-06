094c8135ffbb835256445224f23f57b5485eaf27
Stefan Dürrenberger <sd@pm7.ch>
2026-05-06 08:00:21 +0200

    Update version & build

--- build/blocks/food-quiz/index.asset.php
+++ build/blocks/food-quiz/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '534bf26653079d34834c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3d510916d51923368c19');
