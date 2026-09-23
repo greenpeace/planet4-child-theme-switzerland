b6e8fc493a070b65bb464b574836e227bcb3a79c
Stefan Dürrenberger <sd@pm7.ch>
2026-09-23 08:43:00 +0200

    Revert "build"
    This reverts commit 40d441fe2d8b36d267182a02f9c7e3d10c2b412c.

--- build/blocks/year-end-campaign-2026/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1dca8aa4dd7f986eb511');
--- build/blocks/year-end-campaign-2026/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => '8b173c57943ac5dcea1f');
