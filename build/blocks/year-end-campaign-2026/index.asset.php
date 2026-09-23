c22806854d3034957d100f62c6ce615c497b7c17
Stefan Dürrenberger <sd@pm7.ch>
2026-09-23 08:52:36 +0200

    Update version & build

---
+++ build/blocks/year-end-campaign-2026/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '2a6aedf5d9d3548afc89');
---
+++ build/blocks/year-end-campaign-2026/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '8b173c57943ac5dcea1f');
